07b82a96a4dfc4e96e1907a3b995f1dfe61ad35e
Karel Tuma <karel.tuma@gmail.com>
2012-02-24 23:59:20 +0100

    Implement commands & config files

--- src/main/groovy/sharedbans/SharedBans.groovy
+++ src/main/groovy/sharedbans/SharedBans.groovy
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.plugin.*
 import org.bukkit.event.*
 import org.bukkit.event.player.*
+import org.bukkit.command.*
   
 class SharedBans extends JavaPlugin {
     def desc, ver, info, ipbls, nickbls, bls
@@ -14,15 +15,44 @@ class SharedBans extends JavaPlugin {
         def logger = Logger.getLogger("Minecraft")
         ver = desc.getVersion()
         info = { str -> logger.info("[SharedBans] $str") }
-        getServer().getPluginManager().registerEvents(new SharedBansListener(this), this);
-        info("$ver loaded.")
-        ipbls = ["majncraft.ip.dnsbl.upal.se"]
-        nickbls = ["majncraft.nick.dnsbl.upal.se"]
-        bls = ipbls + nickbls;
+        getServer().getPluginManager().registerEvents(new SharedBansListener(this), this)
+        this.getConfig().options().copyDefaults(true)
+        saveConfig()
+        info("$ver loaded, using ${loadbls()} DNSBLs")
     }
          
     void onDisable() {
         info("v$ver unloaded")
+    }
+
+    int loadbls() {
+        def cfg = this.getConfig()
+        ipbls = cfg.getStringList("ip-dnsbls")
+        nickbls = cfg.getStringList("nick-dnsbls");
+        bls = ipbls + nickbls
+        return bls.size()
+    }
+
+    boolean onCommand(CommandSender sender,Command cmd,String label,String[] args) {
+        def c = cmd.getName().toLowerCase()
+        def spam = { msg -> sender.sendMessage("[SharedBans] $msg"); return true }
+        def cfg = this.getConfig()
+        if (!sender.hasPermission("sb.$c"))
+                return spam("you need sb.$c permission to do that.")
+
+        def wat=false
+        switch (c) {
+            case "sbreload":
+                reloadConfig()
+                return spam("config reloaded, using ${loadbls()} DNSBLs.")
+            case "sbex":
+                wat=true
+            case "sbunex":
+                if (args.length != 1) return false
+                cfg.set("nickexempts.${args[0].toLowerCase()}",wat);
+                saveConfig()
+                return spam("nick exempt for '${args[0]}' ${wat?"added":"removed"}.")
+        }
     }
 }
 
@@ -38,6 +68,7 @@ class SharedBansListener implements Listener {
         def p = e.getPlayer()
         def ip = p.getAddress().getAddress().getHostAddress()
         def name = p.getName()
+        if (plugin.getConfig().get("nickexempts.${name.toLowerCase()}")) return;
         Thread.start {
             for (bl in plugin.bls) {
                 def str = ((bl in plugin.ipbls)?ip:name)+"."+bl
